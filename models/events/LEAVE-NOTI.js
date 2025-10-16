c7393436cef77fd822447a4c000339c6e6d8f7e1
KRISHNA BOT MASTER <ravikumar630344222@gmail.com>
2025-10-16 12:11:42 +0530

    Update and rename 0_LEAVE-NOTI.js to LEAVE-NOTI.js

--- models/events/LEAVE-NOTI.js
+++ models/events/LEAVE-NOTI.js
@@ -21,7 +21,7 @@ module.exports.run = async function({ api, event, Users, Threads }) {
   const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
   const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
   const type = (event.author == event.logMessageData.leftParticipantFbId) ? "खुद ही भाग गया 😐👈" : "एडमिन ने गुस्से में निकाल दिया 😐👈";
-  (typeof data.customLeave == "undefined") ? msg = "┏━━━━━┓\n     ARIF-BABU                    ✧═══•❁😎❁•═══✧\n┗━━━━━┛\n\n\n SUNA HA IS GROUP ME EK THARKI KAM HO GAYA 😀👈\nNAME  𒁍  {name}\nREJAN  𒁍 {type} 🤐✌️\n◆━◆━◆━◆━◆━◆━◆━◆━◆━◆━◆━◆━◆━◆\n\nBYE BYE THARKI INSAAN GOOD{session}\n{time} ♥️♥️" : msg = data.customLeave;
+  (typeof data.customLeave == "undefined") ? msg = "┏━━━━━┓\n     KRISHNA-BABU                    ✧═══•❁😎❁•═══✧\n┗━━━━━┛\n\n\n SUNA HA IS GROUP ME EK THARKI KAM HO GAYA 😀👈\nNAME  𒁍  {name}\nREJAN  𒁍 {type} 🤐✌️\n◆━◆━◆━◆━◆━◆━◆━◆━◆━◆━◆━◆━◆━◆\n\nBYE BYE THARKI INSAAN GOOD{session}\n{time} ♥️♥️" : msg = data.customLeave;
   msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
 
   var link = [  
